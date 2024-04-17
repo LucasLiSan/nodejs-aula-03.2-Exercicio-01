4313081d65ad61a8572703516a10ee5ece4e6dec
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-04-17 17:03:24 -0300

    update index, clientes, css

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.get("/user", (req, res) => {
 //Tela dos clientes
 app.get("/clientes", (req,res) => {
     var clientes = [
-        {nome: "Lucas Lima", cpf: "123.456.789-10", endereco: "Rua 1"}
+        {profilePic: "imgs/profilePic/BillGates.webp" , nome: "William Gates", cpf: "123.456.789-10", endereco: "Rua Seattle"},
+        {profilePic: "imgs/profilePic/LinusTorvalds.webp" ,nome: "Linus Torvalds", cpf: "456.789.012-13", endereco: "Rua Helsinque"},
+        {profilePic: "imgs/profilePic/MarkZuckerberg.webp" ,nome: "Mark Zuckerberg", cpf: "789.012.345-16", endereco: "Rua White Plains"},
+        {profilePic: "imgs/profilePic/SergeyBrin.webp" ,nome: "Sergey Brin", cpf: "012.345.678-19", endereco: "Rua Moscou"},
+        {profilePic: "imgs/profilePic/LawrencePage.webp" ,nome: "Lawrence Page", cpf: "345.678.901-22", endereco: "Rua Lansing"},
+        {profilePic: "imgs/profilePic/JeffreyBezos.webp" ,nome: "Jeffrey Bezos", cpf: "678.901.234-25", endereco: "Rua Albuquerque"},
+        {profilePic: "imgs/profilePic/GabeNewell.webp" ,nome: "Gabe Newell", cpf: "901.234.567-128", endereco: "Rua Colorado"}
     ];
     res.render("clientes", {
         clientes : clientes
