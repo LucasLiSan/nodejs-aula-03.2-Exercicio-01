745e4eb5d129401d70ed41ba5793f22979e8851e
LucasLiSan <lukassantana_rgt@hotmail.com>
2024-04-19 13:37:19 -0300

    end

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.get("/clientes", (req,res) => {
         {profilePic: "imgs/profilePic/SergeyBrin.webp" ,nome: "Sergey Brin", cpf: "012.345.678-19", endereco: "Rua Moscou"},
         {profilePic: "imgs/profilePic/LawrencePage.webp" ,nome: "Lawrence Page", cpf: "345.678.901-22", endereco: "Rua Lansing"},
         {profilePic: "imgs/profilePic/JeffreyBezos.webp" ,nome: "Jeffrey Bezos", cpf: "678.901.234-25", endereco: "Rua Albuquerque"},
-        {profilePic: "imgs/profilePic/GabeNewell.webp" ,nome: "Gabe Newell", cpf: "901.234.567-128", endereco: "Rua Colorado"}
+        {profilePic: "imgs/profilePic/GabeNewell.webp" ,nome: "Gabe Newell", cpf: "901.234.567-28", endereco: "Rua Colorado"},
+        {profilePic: "imgs/profilePic/ShigeruMiyamoto.webp" ,nome: "Shigeru Miyamoto", cpf: "234.567.890-31", endereco: "Rua Sonobe"}
     ];
     res.render("clientes", {
         clientes : clientes
